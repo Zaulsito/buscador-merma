3084f01ea9909341c51fd1ad8e7a426b6156077c
Yamir <yamir.sandoval.zaul@gmail.com>
2026-04-19 02:27:13 -0400

    Fix: Add missing Navbar import in DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -14,6 +14,7 @@ import TraspasosPage from "./TraspasosPage";
 import TutorialOverlay from "../components/TutorialOverlay";
 import InformacionPage from "./InformacionPage";
 import BottomNav from "../components/BottomNav";
+import Navbar from "../components/Navbar";
 
 const modulos = [
   {
